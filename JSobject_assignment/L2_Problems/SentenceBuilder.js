c4eefc3306214d2d4ff979230835177775768543
Prashant23y <23y.prashant@gmail.com>
2025-03-09 09:56:15 +0530

    done

--- JSobject_assignment/L2_Problems/SentenceBuilder.js
+++ JSobject_assignment/L2_Problems/SentenceBuilder.js
@@ -1,5 +1,27 @@
 let sentence = {
     subject: "I",
     verb: "am",
-    object: "The cat",
-}
+    object: "coding",
+
+    sentenceBuilder: function () {
+        if (!this.subject || !this.verb || !this.object) {
+            return "Incomplete sentence";
+        }
+        return this.subject + " " + this.verb + " " + this.object;
+    },
+
+    updateProperty: function (property, value) {
+        if (this.hasOwnProperty(property)) {
+            this[property] = value;
+        } else {
+            return "Invalid property";
+        }
+    }
+};
+
+console.log(sentence.sentenceBuilder());
+
+sentence.updateProperty("verb", "am learning");
+console.log(sentence.sentenceBuilder());
+
+console.log(sentence.updateProperty("mood", "happy"));
